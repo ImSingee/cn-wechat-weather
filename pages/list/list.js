2916972eb940c16e29fa923d4d0ec64124142ecc
Singee <imsingee@gmail.com>
2018-07-21 02:30:56 +0800

    5.7 END

--- pages/list/list.js
+++ pages/list/list.js
@@ -5,14 +5,14 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    futureWeathers: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    this.getFuture()
   },
 
   /**
@@ -47,7 +47,9 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+    this.getFuture( () => {
+      wx.stopPullDownRefresh()
+    })
   },
 
   /**
@@ -62,5 +64,47 @@ Page({
    */
   onShareAppMessage: function () {
   
+  },
+  getFuture(callback){
+    wx.request({
+      url: 'http://test-miniprogram.com/api/weather/future',
+      data: {
+        city: '广州市',
+        time: +new Date()
+      },
+      success: res => {
+        let result = res.data.result
+        
+        let date = new Date()
+
+        for (let i in result) {
+          if (i !== '0') {
+            date.setDate(date.getDate() + 1)
+          }
+          
+          result[i].date = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
+          result[i].day = `星期${{
+            0: '日',
+            1: '一',
+            2: '二',
+            3: '三',
+            4: '四',
+            5: '五',
+            6: '六',
+          }[date.getDay()]}`
+        }
+
+        console.log(result)
+        this.setData({
+          futureWeathers: result
+        })
+      },
+      error: res => {
+        console.warn(res)
+      },
+      complete: res => {
+        callback && callback()
+      }
+    })
   }
 })
